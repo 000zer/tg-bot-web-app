64b5e381c3192dea48e1349b9488fde1aa6b8829
000zer <104787544+000zer@users.noreply.github.com>
2025-10-23 16:44:25 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,61 +7,81 @@ function App() {
 useEffect(()=>{
   tg.ready();
 },[])
-  const onClose=()=>{
-  tg.close();
-}
-
-// Налаштування головної кнопки
-tg.MainButton.text = "Відправити";
-tg.MainButton.color = "#2481cc";
-tg.MainButton.textColor = "#ffffff";
-tg.MainButton.show();
-
-// Обробка натискання
-tg.MainButton.onClick(() => {
+  
+  
+  // Налаштування головної кнопки
+  tg.MainButton.text = "Відправити";
+  tg.MainButton.color = "#2481cc";
+  tg.MainButton.textColor = "#ffffff";
+  tg.MainButton.show();
+  
+  // Обробка натискання
+  tg.MainButton.onClick(() => {
     // Ваш код
     tg.sendData(JSON.stringify({ action: 'submit' }));
-});
-
-// Показ індикатора завантаження
-tg.MainButton.showProgress();
-
-// Приховування індикатора
-tg.MainButton.hideProgress();
-
-// Вимкнення кнопки
-tg.MainButton.disable();
-
-// Приховування кнопки
-tg.MainButton.hide();
-// Показати кнопку назад
-tg.BackButton.show();
-
-// Обробка натискання
-tg.BackButton.onClick(() => {
+  });
+  
+  // Показ індикатора завантаження
+  tg.MainButton.showProgress();
+  
+  // Приховування індикатора
+  tg.MainButton.hideProgress();
+  
+  // Вимкнення кнопки
+  tg.MainButton.disable();
+  
+  // Приховування кнопки
+  tg.MainButton.hide();
+  // Показати кнопку назад
+  tg.BackButton.show();
+  
+  // Обробка натискання
+  tg.BackButton.onClick(() => {
     // Повернутися на попередню сторінку
     window.history.back();
-});
-
-// Приховати кнопку
-tg.BackButton.hide();
-// Показати кнопку налаштувань
-tg.SettingsButton.show();
-
-// Обробка натискання
-tg.SettingsButton.onClick(() => {
+  });
+  
+  // Приховати кнопку
+  tg.BackButton.hide();
+  // Показати кнопку налаштувань
+  tg.SettingsButton.show();
+  
+  // Обробка натискання
+  tg.SettingsButton.onClick(() => {
     // Відкрити налаштування
     console.log('Settings clicked');
-});
-
-// Приховати
-tg.SettingsButton.hide();
-
+  });
+  
+  // Приховати
+  tg.SettingsButton.hide();
+  tg.expand();
+  
+  // Налаштувати MainButton
+  tg.MainButton.text = "Готово";
+  tg.MainButton.show();
+  
+  tg.MainButton.onClick(() => {
+    // Відправити дані боту
+    tg.sendData(JSON.stringify({
+      message: "Користувач натиснув кнопку"
+    }));
+  });
+  
+  // Показати BackButton
+  tg.BackButton.show();
+  tg.BackButton.onClick(() => {
+    tg.close();
+  });
+  
+  // Обробка звичайної кнопки
+  // document.getElementById('myButton').addEventListener('click', () => {
+  //   tg.showAlert('Привіт з Web App!');
+  // });
   return (
     <>
       <div>
-        hay there! this is a telegram web app
-        <button onClick={onClose}>X</button>
+        {/* hay there! this is a telegram web app
+        <button onClick={onClose}>X</button> */}
       </div>
     </>
   )
